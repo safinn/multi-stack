bb7fc7d53b99047abcdb0fdec76a5f4c3fc2a4a6
Dimitris <safinn@users.noreply.github.com>
2025-03-22 14:25:21 +0000

    remove unused ts expected error

--- server/app.ts
+++ server/app.ts
@@ -28,7 +28,6 @@ app.use(httpLogger())
 
 app.use(
   createRequestHandler({
-    // @ts-expect-error - virtual module provided by React Router at build time
     build: () => import('virtual:react-router/server-build'),
     getLoadContext(_, res) {
       return {
